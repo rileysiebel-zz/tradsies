f291faebf4263b160a2f60f5ad557eaa34ae1e72
Riley Siebel <riley.siebel@gmail.com>
2015-02-01 10:38:28 -0800

    signup betterment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,11 @@ class UsersController < ApplicationController
     end
   end
 
-  def user_params
-    params.require(:user).permit(:name, :email);
-                                 # :password,
-                                 # :password_confirmation)
-  end
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email);
+      # :password,
+      # :password_confirmation)
+    end
 end
