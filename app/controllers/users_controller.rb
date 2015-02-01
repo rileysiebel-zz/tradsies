7e32b3de71d1ab069032e2d8ff2841d24bb46077
Riley Siebel <riley.siebel@gmail.com>
2015-02-01 11:42:27 -0800

    some edit page stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  # before_action :logged_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -10,18 +12,37 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Tradsies!"
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
     def user_params
-      params.require(:user).permit(:name, :email);
-      # :password,
-      # :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+  # def logged_in_user
+  #   unless logged_in?
+  #     flash[:danger] = "Please log in."
+  #     redirect_to login_url
+  #   end
+  # end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "test", password_confirmation: "test")
   end
 
   test "should be valid" do
@@ -30,27 +30,31 @@ class UserTest < ActiveSupport::TestCase
   end
 
 
-  # test "email validation should accept valid addresses" do
-  #   valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-  #                        first.last@foo.jp alice+bob@baz.cn]
-  #   valid_addresses.each do |valid_address|
-  #     @user.email = valid_address
-  #     assert @user.valid?, "#{valid_address.inspect} should be valid"
-  #   end
-  # end
+  test "email validation should accept valid addresses" do
+    skip("Traci")
 
-  # test "email validation should reject invalid addresses" do
-  #   invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-  #                          foo@bar_baz.com foo@bar+baz.com]
-  #   invalid_addresses.each do |invalid_address|
-  #     @user.email = invalid_address
-  #     assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
-  #   end
-  # end
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
 
-  # test "email addresses should be unique" do
-  #   duplicate_user = @user.dup
-  #   @user.save
-  #   assert_not duplicate_user.valid?
-  # end
+  test "email validation should reject invalid addresses" do
+    skip("Traci")
+
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
